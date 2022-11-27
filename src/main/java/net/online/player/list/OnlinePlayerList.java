6d8c43d765d492b41ca1ffb738e41c2a46a1a7ca
Misha Opstal <mishaopstal2004@gmail.com>
2022-11-27 17:32:17 +0100

    Basic Fabric Mod

---
+++ src/main/java/net/online/player/list/OnlinePlayerList.java
@@ -0,0 +1,11 @@
+package net.online.player.list;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OnlinePlayerList implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
